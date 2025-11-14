56533b806b96cfc4135b4f81df7a48953e74fe1d
NikkoRagudo <168717963+DevNikz@users.noreply.github.com>
2025-11-14 17:02:58 +0800

    Improved hub

--- Assets/Scripts/Objects/PlanetSelecter.cs
+++ Assets/Scripts/Objects/PlanetSelecter.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlanetSelecter : MonoBehaviour
 {
-    public Transform[] areas;
+    public List<Transform> areas;
     [Range(0.1f, 10f)] public float rotateSpeed = 5f;
     public Transform cam;
+    public List<Material> colorPoints; // 0 - Deselected | 1 - Selected
 
     [SerializeField] private int currentIndex = 0;
 
@@ -22,24 +25,41 @@ public class PlanetSelecter : MonoBehaviour
     void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.W))
-            currentIndex = (currentIndex + 1) % areas.Length;
+            currentIndex = (currentIndex + 1) % areas.Count;
 
         if (Input.GetKeyDown(KeyCode.S))
-            currentIndex = (currentIndex - 1 + areas.Length) % areas.Length;
+            currentIndex = (currentIndex - 1 + areas.Count) % areas.Count;
 
         if (Input.GetKeyDown(KeyCode.A))
-            currentIndex = (currentIndex - 1 + areas.Length) % areas.Length;
+            currentIndex = (currentIndex - 1 + areas.Count) % areas.Count;
 
         if (Input.GetKeyDown(KeyCode.D))
-            currentIndex = (currentIndex + 1) % areas.Length;
+            currentIndex = (currentIndex + 1) % areas.Count;
     }
 
     void RotatePlanet()
     {
         Transform target = areas[currentIndex];
+
+        ClearColor();
+        SetColor(target);
+        
         Vector3 dir = (target.position - transform.position).normalized;
         Vector3 targetForward = (cam.position - transform.position).normalized;
         Quaternion lookRot = Quaternion.FromToRotation(dir, targetForward) * transform.rotation;
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * rotateSpeed);
+    }
+
+    void ClearColor()
+    {
+        for(int i = 0; i < areas.Count; i++)
+        {
+            areas[i].GetComponent<MeshRenderer>().material = colorPoints[0];
+        }
+    }
+
+    void SetColor(Transform target)
+    {
+        target.GetComponent<MeshRenderer>().material = colorPoints[1];
     }
 }
